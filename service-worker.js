ed09ca8b116ab1d27c514a4b23037dec90e85323
Bloodqt <joelgzh-1997@hotmail.com>
2025-09-21 12:23:53 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -2,10 +2,10 @@ self.addEventListener("install", event => {
   event.waitUntil(
     caches.open("calculator-cache").then(cache => {
       return cache.addAll([
-        "./index.html",
-        "./manifest.json",
-        "./icon-192.png",
-        "./icon-512.png"
+        "/RMGCalculator/index.html",
+        "/RMGCalculator/manifest.json",
+        "/RMGCalculator/icon-192.png",
+        "/RMGCalculator/icon-512.png"
       ]);
     })
   );
